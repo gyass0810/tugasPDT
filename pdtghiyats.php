eb80f640f7544eb7cd7f0da689eac5565af4bc78
gyass0810 <104538906+gyass0810@users.noreply.github.com>
2023-09-01 15:26:36 +0700

    Add files via upload

---
+++ pdtghiyats.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TUGAS PDT</title>
+</head>
+<body>
+
+<?php
+$nama = "Ghiyats Alyasa";
+$npm = "217006041";
+$kelas = "Kelas B";
+
+echo "<h1>TUGAS PDT</h1>";
+echo "<p>Nama: " . $nama . "</p>";
+echo "<p>NPM: " . $npm . "</p>";
+echo "<p>Kelas: " . $kelas . "</p>";
+?>
+
+</body>
+</html>
+
+
+
+
+
